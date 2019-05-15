7c9de8fb716c0142e9c556eac63316615fe22076
Antony Budianto <antonybudianto@gmail.com>
2019-05-15 21:53:35 +0700

    use sprintf on main.go

--- starter/cmd/helloapp/main.go
+++ starter/cmd/helloapp/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"starter/app"
 )
 
+const port = 8000
+
 func main() {
 	a := app.App{}
 	a.Initialize("root", "hello", "rest_api_example")
-
-	log.Println("API running at :8000, try http://localhost:8000/users")
-
-	a.Run(":8000")
+	log.Println(fmt.Sprintf("API running at :%v, try http://localhost:%v/users", port, port))
+	a.Run(fmt.Sprintf(":%v", port))
 }
